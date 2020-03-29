8f05aeb1628b50fab5cf80142da83563ef977761
NITISH KUMAR SINGH <nitishk72@gmail.com>
2020-03-29 17:14:55 +0530

    homepage edited

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,30 +11,30 @@
         <div class="row foodTypeCards justify-content-between">
             <div class="container">
              <div class="row justify-content-around my-4">
-             <a href="/product/BreakFast" class="card col-3">
+             <a href="/product/BreakFast" class="card col-4 pb-4">
                     <div style="background-image:url(/img/BreakFast.jpeg)" class="card-img-top" alt="..."></div>
                     <div class="card-body text-center">
-                      <h5 class="card-title">BreakFast</h5>
+                      <h5 class="card-title pt-4">BreakFast</h5>
                     </div>
                 </a>
-                <a href="/product/Lunch" class="card col-3">
+                <a href="/product/Lunch" class="card col-4 pb-4">
                    <div style="background-image:url(/img/Lunch.jpeg)" class="card-img-top" alt="..."></div>
                    <div class="card-body text-center">
-                     <h5 class="card-title">Lunch</h5>
+                     <h5 class="card-title pt-4">Lunch</h5>
                    </div>
                </a>
              </div>
             <div class="row justify-content-around my-4">
-                <a href="/product/Dinner" class="card col-3">
+                <a href="/product/Dinner" class="card col-4 pb-4">
                     <div style="background-image:url(/img/Dinner.jpeg)" class="card-img-top" alt="..."></div>
                     <div class="card-body text-center">
-                      <h5 class="card-title">Dinner</h5>
+                      <h5 class="card-title pt-4">Dinner</h5>
                     </div>
                 </a>
-                <a href="/product/Dessert" class="card col-3">
+                <a href="/product/Dessert" class="card col-4 pb-4">
                     <div style="background-image:url(/img/Dessert.jpeg)" class="card-img-top" alt="..."></div>
                     <div class="card-body text-center">
-                      <h5 class="card-title">Dessert</h5>
+                      <h5 class="card-title pt-4">Dessert</h5>
                     </div>
                 </a>
             </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,28 +7,28 @@
                 <div class="col-sm backImg welcomeImg "></div>
                 <div class="col-sm text-light contentDiv">
                     <div class="welcomeText">
-                        <h2 class="title" >Our Restaurant in your pocket </h2>
-                        <p class="subtitle">Order from your favorite restaurants & track on the go, with the all-new Swiggy app.</p>
+                        <h2 class="display-3 my-font text-white mb-4" >Our Restaurant in your pocket </h2>
+                        <p class="title">Order from your favorite restaurants & track on the go, with the all-new Swiggy app.</p>
                     </div>
                 </div>
             </div>
             <div class="row welcomeCards">
                <div class="d-flex justify-content-around">
-                <div class="card">
+                <div class="card" style="min-width:25%;min-height:200px">
                     <img src="/img/minOrder.svg" class="card-img-top" alt="...">
                     <div class="card-body text-center">
-                      <h5 class="card-title">No Minimum Order</h5>
+                      <h5 class="card-title ">No Minimum Order</h5>
                       <p class="card-text">Order in for yourself or for the group, with no restrictions on order value</p>
                     </div>
                 </div>
-                <div class="card">
+                <div class="card" style="min-width:25%;min-height:200px">
                     <img src="/img/liveLoc.svg" class="card-img-top" alt="...">
                     <div class="card-body text-center">
                       <h5 class="card-title">Live Order Tracking</h5>
                       <p class="card-text">Know where your order is at all times, from the restaurant to your doorstep</p>
                     </div>
                 </div>
-                <div class="card">
+                <div class="card" style="min-width:25%;min-height:200px">
                     <img src="/img/onTheWay.svg" class="card-img-top" alt="...">
                     <div class="card-body text-center">
                       <h5 class="card-title">Lightning-Fast Delivery</h5>
@@ -40,8 +40,8 @@
             <div class="row welcome">
                 <div class="col-sm text-light contentDiv">
                     <div class="welcomeText">
-                        <h2 class="title" >Our Restaurant in your pocket </h2>
-                        <p class="subtitle">Order from your favorite restaurants & track on the go, with the all-new Swiggy app.</p>
+                        <h2 class="display-3 my-font text-white mb-4" >Our Restaurant in your pocket </h2>
+                        <p class="title">Order from your favorite restaurants & track on the go, with the all-new Swiggy app.</p>
                     </div>
                 </div>
                 <div class="col-sm backImg welcomeImg" ></div>
